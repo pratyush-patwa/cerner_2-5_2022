44c4c0f021f2aad42a7b4475df42cbb07eaa64ea
Pratyush Patwa <pratyush.patwa@gmail.com>
2022-09-16 19:59:37 +0530

    Day2 in C

---
+++ TwoPowerFive.c
@@ -0,0 +1,10 @@
+//cerner_2tothe5th_2022
+#include <stdio.h>
+int main()
+{
+    int num =2;
+    for(int i =1; i<5; i++ ){
+        num *= 2;
+    }
+    printf("The two's power 5 is %d ", num);
+}
