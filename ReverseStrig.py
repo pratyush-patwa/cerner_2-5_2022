e640bc18aaf29700eacf24b312dcdd21c394885c
Pratyush Patwa <pratyush.patwa@gmail.com>
2022-09-17 19:25:50 +0530

    Day3 in python

---
+++ ReverseStrig.py
@@ -0,0 +1,8 @@
+#cerner_2tothe5th_2022
+def reverse(str):   
+    str = str[::-1]   
+    return str   
+    
+s = "OracleCerner"  
+print ("The original string is : ",s)   
+print ("The reversed string is : ",reverse(s))  
