57b158469cf0c9387d10adc69432e83c90f32d21
Pratyush Patwa <pratyush.patwa@gmail.com>
2022-09-20 22:58:09 +0530

    Day 6 Go

---
+++ addition.go
@@ -0,0 +1,16 @@
+//cerner_2tothe5th_2022
+package main
+
+import "fmt"
+
+func main() {
+
+   var number1, number2, number3 int
+   
+   number1 = 100
+   number2 = 200
+   
+   number3 = number1 + number2
+
+   fmt.Println("The addition of ", number1, " and ", number2, " is \n ", number3, "\n(Addition of two integers within the function)")
+}
