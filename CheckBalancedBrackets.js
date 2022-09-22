22cc95111a364ee48dd2e3c0c6111db0166253eb
Pratyush Patwa <pratyush.patwa@gmail.com>
2022-09-22 23:04:04 +0530

    Day 8 in javascript

---
+++ CheckBalancedBrackets.js
@@ -0,0 +1,19 @@
+//cerner_2tothe5th_2022
+let parenMatcher = (str) =>{
+	let brackets = [], bracketsMap = {'[':']', '(':')', '{':'}'};
+	for(let i=0; i<str.length; i++){
+		if(['[', '(', '{'].includes(str[i])){
+			brackets.push(str[i])
+		}
+		if([']', ')', '}'].includes(str[i])){
+			let last = brackets.pop();
+			if(bracketsMap[last] !== str[i])
+				return false
+		}
+	}
+    if(brackets.length)
+    return false;
+	return true;
+}
+console.log(parenMatcher('[{{{{[[[]]'));
+console.log(parenMatcher('[[()]]'))
