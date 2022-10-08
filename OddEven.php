3260b3cc05c832bdf83d232e9f24e50fa7af35a5
Pratyush Patwa <pratyush.patwa@gmail.com>
2022-10-08 22:52:03 +0530

    Create OddEven.php

---
+++ OddEven.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+# cerner_2tothe5th_2022
+$i;
+for ($i = 1; $i <= 100; $i++)
+{
+	if ($i % 2 == 0)
+		echo "Even" . " ";		
+
+	else 		
+		echo "Odd" . " ";			
+}
+?>
+</body>
+</html>
