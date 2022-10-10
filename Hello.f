9cb4e14378ef4930d34881aa651a74314f5c66d5
Pratyush Patwa <pratyush.patwa@gmail.com>
2022-10-10 23:19:49 +0530

    Update and rename hello.f to Hello.f

---
+++ Hello.f
@@ -0,0 +1,4 @@
+! cerner_2tothe5th_2022
+Program Hello
+Print *, "Hello World"
+End Program Hello
--- hello.f
+++
@@ -1,4 +0,0 @@
-! cerner_2tothe5th_2022
-program hello
-   Print *, "Hello World!"
-end program Hello
