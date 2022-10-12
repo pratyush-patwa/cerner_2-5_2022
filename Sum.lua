69ef4ca69f675807c577d3ca35493cae7df64531
Pratyush Patwa <pratyush.patwa@gmail.com>
2022-10-12 23:22:48 +0530

    Create Sum.lua

---
+++ Sum.lua
@@ -0,0 +1,8 @@
+--cerner_2tothe5th_2022
+local num1, num2
+
+print("Enter num1 : ")
+num1 =  io.read("*n")
+print("Enter num2 : ")
+num2 =  io.read("*n")
+print("Sum of num1 and num2 : ", num1+num2)
