564752f0af686f6938f64b862106b3db14caf4bd
Pratyush Patwa <pratyush.patwa@gmail.com>
2022-10-12 22:52:59 +0530

    Create Product.d

---
+++ Product.d
@@ -0,0 +1,14 @@
+////cerner_2tothe5th_2022
+import std.stdio;
+
+void main()
+{
+  
+    int num1, num2;
+    writef("Enter  a number: ");
+    readf(" %s", &num1);
+    writef("Enter  another number: ");
+    readf(" %s", &num2);
+    writef( "Product is " );
+    writeln(num1* num2);
+}
