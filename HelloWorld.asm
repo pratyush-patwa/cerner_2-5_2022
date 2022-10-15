e96f4783db89d9b8f98c427bbb3ba54c1faf3d30
Pratyush Patwa <pratyush.patwa@gmail.com>
2022-10-15 14:53:14 +0530

    Create HelloWorld.asm

---
+++ HelloWorld.asm
@@ -0,0 +1,17 @@
+;cerner_2tothe5th_2022
+
+.model small
+.data
+        msg db 10d,13d,"Hello World$"
+
+.code
+        mov ax,@data
+        mov ds,ax
+                                        
+        lea dx,msg                      
+        mov ah,09h                      
+        int 21h                         
+
+        mov ah,4ch                      
+        int 21h                         
+end
